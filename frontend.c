9d431126487779f0a3c430da889290e3f037de7f
xfnty <xfntyx@gmail.com>
2025-06-28 11:55:53 -0800

    Basic window

--- frontend.c
+++ frontend.c
@@ -1,3 +1,25 @@
+/* defines */
+#define true 1u
+#define false 0
+#define WNDCLASS_NAME "libretro-frontend"
+#define INVALID_HANDLE_VALUE  (ptr)-1
+#define ATTACH_PARENT_PROCESS (u32)-1
+#define STD_OUTPUT_HANDLE     (u32)-11
+#define IDC_ARROW             ((cstr)32512)
+#define WS_OVERLAPPEDWINDOW   0x00CF0000L
+#define CW_USEDEFAULT         0x80000000
+#define SW_SHOW               5
+#define SW_HIDE               0
+#define WM_CLOSE              0x0010u
+#define PM_REMOVE             0x0001u
+
+#if defined(_MSC_VER)
+    #define WINAPI __stdcall
+#else
+    #define WINAPI __attribute__((stdcall))
+#endif
+
+
 /* typedefs */
 typedef char               c8;
 typedef char               i8;
@@ -14,19 +36,38 @@ typedef const char        *cstr;
 typedef void              *ptr;
 typedef unsigned char     *va_list;
 
-/* defines */
-#define true 1u
-#define false 0
 
-#if defined(_MSC_VER)
-    #define WINAPI __stdcall
-#else
-    #define WINAPI __attribute__((stdcall))
-#endif
+/* imported typedefs */
+typedef i64 (WINAPI *WNDPROC)(ptr hwnd, u32 msg, u64 wp, i64 lp);
+typedef struct WNDCLASSA WNDCLASSA;
+struct WNDCLASSA {
+    u32     style;
+    u8      _padding[4];
+    WNDPROC lpfnWndProc;
+    i32     cbClsExtra;
+    i32     cbWndExtra;
+    ptr     hInstance;
+    ptr     hIcon;
+    ptr     hCursor;
+    ptr     hbrBackground;
+    cstr    lpszMenuName;
+    cstr    lpszClassName;
+};
+typedef struct POINT POINT;
+struct POINT {
+    i32 x, y;
+};
+typedef struct MSG MSG;
+struct MSG {
+    ptr   hwnd;
+    u32   message;
+    u64   wParam;
+    i32   lParam;
+    u32   time;
+    POINT pt;
+    u32   lPrivate;
+};
 
-#define INVALID_HANDLE_VALUE  (ptr)-1
-#define ATTACH_PARENT_PROCESS (u32)-1
-#define STD_OUTPUT_HANDLE     (u32)-11
 
 /* macros */
 #define countof(_a) (sizeof(_a)/sizeof((_a)[0]))
@@ -34,35 +75,86 @@ typedef unsigned char     *va_list;
 #define va_arg(_list, _T) ( ( sizeof(_T) > 8) ? (**(_T**)((_list += 8) - 8)) : ( *(_T* )((_list += 8) - 8)) )
 
 #if defined(_MSC_VER)
-    #define assert(_x) do { if (!(_x)) __debugbreak(); } while (0)
+    #define assert(_x, _m, ...) do { \
+            if (!(_x)) { \
+                print("error: " _m, ##__VA_ARGS__); \
+                __debugbreak(); \
+            } \
+        } while (0)
 #else
-    #define assert(_x) do { if (!(_x)) { asm("int3"); __builtin_unreachable(); } } while (0)
+    #define assert(_x, _m, ...) do { \
+            if (!(_x)) { \
+                print("error: " _m, ##__VA_ARGS__); \
+                __asm__("int3"); \
+                __builtin_unreachable(); \
+            } \
+        } while (0)
 #endif
 
+
 /* imports */
+u32  WINAPI AttachConsole(u32 pid);
+ptr  WINAPI GetStdHandle(u32 id);
 void ExitProcess(u32 code);
-u32 GetLastError(void);
-u32 WINAPI AttachConsole(u32 pid);
-ptr WINAPI GetStdHandle(u32 id);
-u32 WriteConsoleA(ptr console, ptr buffer, u32 bytes, u32 *written, ptr _reserved);
+u32  GetLastError(void);
+u32  WriteConsoleA(ptr console, ptr buffer, u32 bytes, u32 *written, ptr _reserved);
 void OutputDebugStringA(cstr str);
+ptr  GetModuleHandleA(cstr name);
+ptr  LoadCursorA(ptr hinst, cstr name);
+u16  RegisterClassA(WNDCLASSA *cls);
+i64  DefWindowProcA(ptr hwnd, u32 msg, u64 wp, i64 lp);
+ptr CreateWindowExA(
+    u32  exstyle,
+    cstr cls,
+    cstr title,
+    u32  style,
+    i32  x,
+    i32  y,
+    i32  w,
+    i32  h,
+    ptr  hwnd_parent,
+    ptr  hmenu,
+    ptr  hinst,
+    ptr  lp
+);
+u32 ShowWindow(ptr hwnd, i32 show);
+u32 PeekMessageA(MSG *msg, ptr hwnd, u32 fmin, u32 fmax, u32 flags);
+u32 TranslateMessage(MSG *msg);
+u32 DispatchMessageA(MSG *msg);
+
 
 /* function declarations */
 void _start(void);
 void init_logging(void);
-void info(cstr format, ...);
+void init_ui(void);
+void process_ui_events(void);
+i64 WINAPI window_event_handler(ptr hwnd, u32 msg, u64 wp, i64 lp);
+void print(cstr format, ...);
+void die(cstr format, ...);
 u32 snprintf(c8 *buffer, u32 maxsize, cstr format, ...);
 u32 vsnprintf(c8 *buffer, u32 maxsize, cstr format, va_list args);
 
+
 /* variables */
-ptr stdout;
-int _fltused=1;
+u32 _fltused=1;
+ptr g_stdout;
+struct {
+    ptr hwnd;
+    ptr hinst;
+    u32 was_exit_requested;
+} g_ui;
+
 
 /* function definitions */
 void _start(void)
 {
     init_logging();
-    info("Hello, %llu %hhu %hx %s %f %llf", -1LL, 256, 0x10AB0, "text", -1.1234f, 1.7976931348623157E+16);
+    init_ui();
+
+    while (!g_ui.was_exit_requested)
+    {
+        process_ui_events();
+    }
 
     ExitProcess(0);
 }
@@ -70,19 +162,93 @@ void _start(void)
 void init_logging(void)
 {
     AttachConsole(ATTACH_PARENT_PROCESS);
-    stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    g_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
 }
 
-void info(cstr format, ...)
+void init_ui(void)
+{
+    g_ui.hinst = GetModuleHandleA(0);
+
+    WNDCLASSA wndclass = {
+        .hInstance = g_ui.hinst,
+        .lpszClassName = WNDCLASS_NAME,
+        .lpfnWndProc = window_event_handler,
+        .hCursor = LoadCursorA(0, IDC_ARROW),
+    };
+    assert(RegisterClassA(&wndclass), "RegisterClassA() failed (%d)", GetLastError());
+
+    g_ui.hwnd = CreateWindowExA(
+        0,
+        WNDCLASS_NAME,
+        "Tiny Libretro Frontend",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        0,
+        0,
+        g_ui.hinst,
+        0
+    );
+    assert(g_ui.hwnd, "CreateWindowExA() failed (%d)", GetLastError());
+
+    ShowWindow(g_ui.hwnd, SW_SHOW);
+}
+
+void process_ui_events(void)
+{
+    for (MSG msg; PeekMessageA(&msg, g_ui.hwnd, 0, 0, PM_REMOVE) != 0;)
+    {
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
+    }
+}
+
+i64 window_event_handler(ptr hwnd, u32 msg, u64 wp, i64 lp)
+{
+    if (hwnd != g_ui.hwnd)
+        return DefWindowProcA(hwnd, msg, wp, lp);
+
+    switch (msg)
+    {
+    case WM_CLOSE:
+        g_ui.was_exit_requested = true;
+        ShowWindow(g_ui.hwnd, SW_HIDE);
+        break;
+    }
+    
+    return DefWindowProcA(hwnd, msg, wp, lp);
+}
+
+void print(cstr format, ...)
 {
     c8 buffer[1024];
 
     va_list args;
     va_start(args, format);
-    u32 len = vsnprintf(buffer, sizeof(buffer), format, args);
+    u32 len = vsnprintf(buffer, sizeof(buffer) - 2, format, args);
+    buffer[len++] = '\n';
+    buffer[len++] = '\0';
 
-    WriteConsoleA(stdout, buffer, len, 0, 0);
+    WriteConsoleA(g_stdout, buffer, len, 0, 0);
     OutputDebugStringA(buffer);
+}
+
+void die(cstr format, ...)
+{
+    c8 buffer[1024];
+
+    u32 len = snprintf(buffer, sizeof(buffer), "error: ");
+
+    va_list args;
+    va_start(args, format);
+    len = vsnprintf(buffer + len, sizeof(buffer) - len - 2, format, args);
+    buffer[len++] = '\n';
+    buffer[len++] = '\0';
+
+    WriteConsoleA(g_stdout, buffer, len, 0, 0);
+    OutputDebugStringA(buffer);
+
+    ExitProcess(1);
 }
 
 u32 snprintf(c8 *buffer, u32 maxsize, cstr format, ...)
